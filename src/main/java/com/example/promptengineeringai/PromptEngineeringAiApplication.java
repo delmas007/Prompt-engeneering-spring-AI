9a170e3b245715ba01563af7d4b78b7ddab70822
delmas007 <azertyazerty1ze@gmail.com>
2024-03-07 10:31:44 +0000

    commit

---
+++ src/main/java/com/example/promptengineeringai/PromptEngineeringAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.promptengineeringai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptEngineeringAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromptEngineeringAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/promptengineeringai/PromptEngineeringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.promptengineeringai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptEngineeringAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
