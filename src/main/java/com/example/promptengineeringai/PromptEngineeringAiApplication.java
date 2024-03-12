a2bc0249b151adf0ef591f474dbc3d276fac3015
delmas007 <azertyazerty1ze@gmail.com>
2024-03-12 20:57:28 +0000

    test 1

--- src/main/java/com/example/promptengineeringai/PromptEngineeringAiApplication.java
+++ src/main/java/com/example/promptengineeringai/PromptEngineeringAiApplication.java
@@ -2,8 +2,10 @@ package com.example.promptengineeringai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:Key/.env")
 public class PromptEngineeringAiApplication {
 
     public static void main(String[] args) {
